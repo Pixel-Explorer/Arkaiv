8d46597f7d0094aa7abcb14ad5e4ce7ffb391e26
Anirudh Venkatesan <1991anirudh@gmail.com>
2025-06-11 22:16:27 +0530

    Add webpack externals to skip bufferutil and utf-8-validate

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
+const nodeExternals = require('webpack-node-externals');
+
 const nextConfig = {
   output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  webpack: (config, { isServer }) => {
+    if (isServer) config.externals.push(nodeExternals());
+    return config;
+  },
 };
 
 module.exports = nextConfig;
